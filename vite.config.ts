0c4391afc8c070609c62571fb3bf350e2e79cdb3
Norman <37084749+xuxiguo@users.noreply.github.com>
2025-08-26 16:37:02 -0500

    Update base path for correct GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fin3010-tax-visualizer/', // GitHub Pages URL path
+  base: '/Tax-Visualizer/', // GitHub Pages URL path
 })
